90ffc5e84e469280ab5ec722685e4ebfd5419089
Gabrielle Katherine Marie Stewart <gkstewart@goldengate.cs.brynmawr.edu>
2025-02-15 14:47:58 -0500

    A04 complete

--- A04/magic_square.c
+++ A04/magic_square.c
@@ -1,10 +1,84 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Gabrielle Stewart 
+ * Date: Febuary 12th, 2025
+ * Description: A program that tests whether a given matrix is a magic square. 
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+
+int is_magic_square(int **matrix, int n, int magic_constant) {
+    int sum;
+
+    for (int i = 0; i < n; i++) {
+        sum = 0;
+        for (int j = 0; j < n; j++) {
+            sum += matrix[i][j];
+        }
+        if (sum != magic_constant) return 0;
+    }
+
+    for (int j = 0; j < n; j++) {
+        sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += matrix[i][j];
+        }
+        if (sum != magic_constant) return 0;
+    }
+
+    sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += matrix[i][i];
+    }
+    if (sum != magic_constant) return 0;
+
+    sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += matrix[i][n - 1 - i];
+    }
+    if (sum != magic_constant) return 0;
+
+    return 1;
+}
 
 int main() {
-  return 0;
+    int n, m;
+    if (scanf("%d %d", &n, &m) != 2 || n != m) {
+        printf("Invalid input. Matrix must be square.\n");
+        return 1;
+    }
+
+    // malloc
+    int **matrix = (int **)malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++) {
+        matrix[i] = (int *)malloc(n * sizeof(int));
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (scanf("%d", &matrix[i][j]) != 1) {
+                printf("Invalid input.\n");
+                return 1;
+            }
+        }
+    }
+
+    int magic_constant = 0;
+    for (int j = 0; j < n; j++) {
+        magic_constant += matrix[0][j];
+    }
+
+    if (is_magic_square(matrix, n, magic_constant)) {
+        printf("M is a magic square (magic constant = %d)\n", magic_constant);
+    } else {
+        printf("M is NOT a magic square!\n");
+    }
+
+    // free  memory
+    for (int i = 0; i < n; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+
+    return 0;
 }
+
--- A04/sorted_snacks.c
+++ A04/sorted_snacks.c
@@ -1,10 +1,98 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author:Gabby Stewart 
+ * Date: February 12,2025
+ * Description: a program that allows users to add snacks to the snackbar and displays them in alphabetical order. 
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Snack {
+    char name[50];
+    float cost;
+    int quantity;
+    struct Snack *next;
+} Snack;
+
+Snack *insert_first(Snack *head, char *name, float cost, int quantity) {
+    Snack *new_snack = (Snack *)malloc(sizeof(Snack));
+    if (!new_snack) {
+        printf("Memory allocation failed!\n");
+        return head;
+    }
+    strcpy(new_snack->name, name);
+    new_snack->cost = cost;
+    new_snack->quantity = quantity;
+    new_snack->next = head;
+    return new_snack;
+}
+
+void clear(Snack *head) {
+    Snack *temp;
+    while (head) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+void printList(Snack *head) {
+    int index = 0;
+    printf("\nWelcome to Sorted Linked List Snack Bar.\n\n");
+    Snack *current = head;
+    while (current) {
+        printf("%d) %-20s cost: $%.2f     quantity: %d\n", index++, current->name, current->cost, current->quantity);
+        current = current->next;
+    }
+}
+
+Snack *sortName(Snack *head) {
+    if (!head || !head->next) return head;
+    Snack *sorted = NULL;
+    Snack *current = head;
+    while (current) {
+        Snack *next = current->next;
+        if (!sorted || strcmp(current->name, sorted->name) < 0) {
+            current->next = sorted;
+            sorted = current;
+        } else {
+            Snack *temp = sorted;
+            while (temp->next && strcmp(current->name, temp->next->name) > 0) {
+                temp = temp->next;
+            }
+            current->next = temp->next;
+            temp->next = current;
+        }
+        current = next;
+    }
+    return sorted;
+}
 
 int main() {
-  return 0;
+    int num_snacks;
+    printf("Enter a number of snacks: ");
+    scanf("%d", &num_snacks);
+
+    Snack *snack_list = NULL;
+    for (int i = 0; i < num_snacks; i++) {
+        char name[50];
+        float cost;
+        int quantity;
+
+        printf("Enter a name: ");
+        scanf("%s", name);
+        printf("Enter a cost: ");
+        scanf("%f", &cost);
+        printf("Enter a quantity: ");
+        scanf("%d", &quantity);
+
+        snack_list = insert_first(snack_list, name, cost, quantity);
+    }
+
+    snack_list = sortName(snack_list);
+    printList(snack_list);
+    clear(snack_list);
+
+    return 0;
 }
+
